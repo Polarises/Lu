5590b2a9c7f11fd555f0cd6b8bb60caf106f692e
DT <polarises@live.com>
2022-05-14 02:22:33 +0800

    完成了登录也米娜

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../components/Home.vue'
+import Login from '../components/Login'
+
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/home', component: Home },
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数， 表示放行   ①next()放行   ②next('/login')强制跳转
+  if (to.path === '/login') return next() // 访问登录页，直接放行
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login') // 没有token 强制跳转
+  next() // 否则(有token)直接放行
 })
 
 export default router
